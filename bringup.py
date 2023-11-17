42e4c41c0cb721ff4b8272b17f986bd67646d625
Ben Morris <benmor2020@gmail.com>
2023-11-17 14:14:03 -0500

    Quality of life in bringup.py

--- bringup.py
+++ bringup.py
@@ -22,26 +22,50 @@ class RobotInfo:
 
 def process_command(command: str, robot_info: List[RobotInfo]):
     # command is of the form kill [number]
+    if command == 'exit':
+        for num_to_kill, info in enumerate(robot_info):
+            if not info.running:
+                continue
+            print(f'Killing robot {num_to_kill} with PID {robot_info[num_to_kill].pid}')
+            os.kill(info.pid, signal.SIGTERM)
+            print('Done')
+            info.running = False
+        return True
     num_to_kill = int(command.split(' ')[-1])
     if robot_info[num_to_kill].running:
+        print(f'Killing robot {num_to_kill} with PID {robot_info[num_to_kill].pid}')
         os.kill(robot_info[num_to_kill].pid, signal.SIGTERM)
+        print('Done')
         robot_info[num_to_kill].running = False
+    return not any(r.running for r in robot_info)
+
+
+def get_actual_addr(base_addr):
+    if '192.168' in base_addr:
+        return base_addr
+    if '.' in base_addr:
+        return f'192.168.{base_addr}'
+    return f'192.168.16.{base_addr}'
 
 
 def main():
     ip_addrs = sys.argv[1:]
     robot_info = []
-    for idx, addr in enumerate(ip_addrs):
+    for idx, base_addr in enumerate(ip_addrs):
+        addr = get_actual_addr(base_addr)
         child_id = os.fork()
         if child_id == 0:
             subprocess.run(f'ros2 launch neato_node2 bringup_multi.py host:={addr} udp_sensor_port:={FIRST_SENSOR_PORT + idx} udp_video_port:={FIRST_CAMERA_PORT + idx} robot_name:=robot{idx}'.split(' '),
                            stdout=subprocess.DEVNULL)
         else:
+            print(f'Spawning PID {child_id} for robot {idx} ({addr})')
             robot_info.append(RobotInfo(idx, addr, child_id))
     
     while True:
-        command = input('Enter the command you want to run ')
-        process_command(command, robot_info)
+        command = input('Enter the command you want to run ').strip().lower()
+        if process_command(command, robot_info):
+            print('All robots disconnected')
+            break
 
 
 if __name__ == '__main__':
