be0f118187c8da71affd8c7ad74a510fc529d256
Ben Morris <benmor2020@gmail.com>
2023-11-28 13:49:55 -0500

    Bringup modifications

--- bringup.py
+++ bringup.py
@@ -1,8 +1,7 @@
 
 from typing import *
 import sys
-import os
-import signal
+from multiprocessing import Process
 import subprocess
 
 
@@ -11,11 +10,10 @@ FIRST_CAMERA_PORT = 8888
 
 
 class RobotInfo:
-    def __init__(self, robot_idx: int, addr: str, pid: int):
+    def __init__(self, robot_idx: int, addr: str, proc: Process):
         self.robot_idx = robot_idx
         self.addr = addr
-        self.pid = pid
-        self.running = True
+        self.proc = proc
         self.sensor_port = FIRST_SENSOR_PORT + robot_idx
         self.camera_pot = FIRST_CAMERA_PORT + robot_idx
 
@@ -24,20 +22,17 @@ def process_command(command: str, robot_info: List[RobotInfo]):
     # command is of the form kill [number]
     if command == 'exit':
         for num_to_kill, info in enumerate(robot_info):
-            if not info.running:
+            if not info.proc.is_alive:
                 continue
-            print(f'Killing robot {num_to_kill} with PID {robot_info[num_to_kill].pid}')
-            os.kill(info.pid, signal.SIGTERM)
-            print('Done')
-            info.running = False
+            print(f'Killing robot {num_to_kill}')
+            robot_info[num_to_kill].proc.terminate()
         return True
+    
     num_to_kill = int(command.split(' ')[-1])
-    if robot_info[num_to_kill].running:
-        print(f'Killing robot {num_to_kill} with PID {robot_info[num_to_kill].pid}')
-        os.kill(robot_info[num_to_kill].pid, signal.SIGTERM)
-        print('Done')
-        robot_info[num_to_kill].running = False
-    return not any(r.running for r in robot_info)
+    if robot_info[num_to_kill].proc.is_alive:
+        print(f'Killing robot {num_to_kill}')
+        robot_info[num_to_kill].proc.terminate()
+    return not any(r.proc.is_alive for r in robot_info)
 
 
 def get_actual_addr(base_addr):
@@ -48,18 +43,19 @@ def get_actual_addr(base_addr):
     return f'192.168.16.{base_addr}'
 
 
+def bringup_neato(addr, idx):
+    subprocess.run(f'ros2 launch neato_node2 bringup_multi.py host:={addr} udp_sensor_port:={FIRST_SENSOR_PORT + idx} udp_video_port:={FIRST_CAMERA_PORT + idx} robot_name:=robot{idx}'.split(' '),
+                    stdout=subprocess.DEVNULL)
+
+
 def main():
     ip_addrs = sys.argv[1:]
     robot_info = []
     for idx, base_addr in enumerate(ip_addrs):
         addr = get_actual_addr(base_addr)
-        child_id = os.fork()
-        if child_id == 0:
-            subprocess.run(f'ros2 launch neato_node2 bringup_multi.py host:={addr} udp_sensor_port:={FIRST_SENSOR_PORT + idx} udp_video_port:={FIRST_CAMERA_PORT + idx} robot_name:=robot{idx}'.split(' '),
-                           stdout=subprocess.DEVNULL)
-        else:
-            print(f'Spawning PID {child_id} for robot {idx} ({addr})')
-            robot_info.append(RobotInfo(idx, addr, child_id))
+        proc = Process(target=bringup_neato, args=(addr, idx))
+        proc.start()
+        robot_info.append(RobotInfo(idx, addr, proc))
     
     while True:
         command = input('Enter the command you want to run ').strip().lower()
