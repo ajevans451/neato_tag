d3f7aa836d4d817d4f87396342a1bd797490874b
Ben Morris <benmor2020@gmail.com>
2023-11-29 21:51:17 -0500

    Made neato_tag a ros package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'neato_tag'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bmorris',
+    maintainer_email='benmor2020@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'allconnect = neato_tag.allconnect:main',
+            'lidar_detector = neato_tag.lidar_detector:main',
+        ],
+    },
+)
