7f9c91e68477d237d640f672fff4c0f6e8efcfe4
Jack Levitsky <jlevitsky@olin.edu>
2023-12-09 15:39:27 -0500

    Added to Navigator class

--- neato_tag/navigator.py
+++ neato_tag/navigator.py
@@ -5,13 +5,52 @@ from rclpy.context import Context
 from rclpy.node import Node
 from rclpy.parameter import Parameter
 import numpy as np
+from nav2_msgs.msg import ParticleCloud, Particle
 from geometry_msgs.msg import Twist, Vector3
+
+
+turn_constant=5
 
 class Navigator(Node):
 
-    def __init__(self, image_topic, lidar_topic):
+    def __init__(self):
         super().__init__('navigator')
+
+        self.camera_subscriber=self.create_subscription(ParticleCloud, 'neatos_in_camera', self.find_heading,10)
+        self.lidar_subscriber=self.create_subscription(ParticleCloud,  'neatos_in_lidar', self.find_heading,10)
+        self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.it_status=False
+
+    def run_loop(self,msg):
+
+        nearest_neato_distance=[]
+        nearest_neato_angle=[]
+        self.drive(nearest_neato_angle,nearest_neato_distance)
         
-        self.sub = self.create_subscription(Twist, 'cmd_vel')
-        self.camera_subscriber=self.create_subscription(CameraDetector, image_topic, 10)
-        self.camera_subscriber=self.create_subscription(LidarDetector, lidar_topic, 10)
+    def drive(self,angle, distance):
+        """
+        Takes in angle and distance to the nearest neato and commands Neato motors to head towards or away from that position
+        """
+        if self.it_status:
+            forward_constant=.5
+        else:
+            forward_constant=-.5
+        drive_msg = Twist()
+        drive_msg.linear.x = np.clip(float(forward_constant*distance), -1.0, 1.0)    #Determines final linear drive message
+        drive_msg.angular.z = np.clip(float(turn_constant*angle), -1.0, 1.0)         #Determines final angular drive message
+        self.pub.publish(drive_msg) #Commands Neato motors
+        return
+        
+
+
+        
+
+def main(args=None):
+    rclpy.init()
+    n = Navigator()
+    rclpy.spin(n)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
