f7a12c270570203165356db66281e6bd6914cb27
Ben Morris <benmor2020@gmail.com>
2023-12-13 15:03:49 -0500

    Run away code

--- neato_tag/navigator.py
+++ neato_tag/navigator.py
@@ -10,33 +10,44 @@ FORWARD_ALPHA = 1000.0
 LIDAR_ALPHA = 1
 CAMERA_ALPHA = 1000
 TURN_CONSTANT=5
+FORWARD_CONSTANT = 0.15
 neato_id=1
 
 class Navigator(Node):
 
     def __init__(self,neato_id):
         super().__init__('navigator')
-
-        self.camera_subscriber=self.create_subscription(ParticleCloud, 'neatos_in_camera', self.camera_callback,10)
-        self.it_status_subscriber=self.create_subscription(Int8,'it_status', self.check_it_status, 10)
-        self.lidar_subscriber=self.create_subscription(LaserScan,  'scan', self.avoid_obstacles,10)
+        self.camera_subscriber = self.create_subscription(ParticleCloud, 'neatos_in_camera', self.camera_callback, 10)
+        self.it_status_subscriber = self.create_subscription(Int8, 'it_status', self.check_it_status, 10)
+        self.lidar_subscriber = self.create_subscription(LaserScan, 'scan', self.avoid_obstacles, 10)
         self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.lidar_gradient=np.zeros((2,))
-        self.cam_gradient=np.zeros((2,))
-        self.it_status=True
-        self.neato_id=neato_id
+        self.lidar_gradient = np.zeros((2,))
+        self.cam_gradient = np.zeros((2,))
+        self.it_status = False
+        self.neato_id = neato_id
         self.create_timer(0.03, self.run_loop)
-    
-
 
     def run_loop(self):
-        final_gradient = self.cam_gradient + self.lidar_gradient + np.array([FORWARD_ALPHA / 500, 0])
-        # print(f'{self.lidar_gradient = }, {self.cam_gradient = }, {final_gradient = }')
-        # print(self.cam_gradient)
+        drive_msg = Twist()
+        forward_gradient = np.array([FORWARD_ALPHA / 500, 0])
+        if self.it_status:
+            final_gradient = self.cam_gradient + self.lidar_gradient + forward_gradient
+        else:
+            final_gradient = -self.cam_gradient + self.lidar_gradient - forward_gradient
         target_distance = np.hypot(final_gradient[0], final_gradient[1])
         target_angle = -np.arctan2(final_gradient[1], final_gradient[0])
+        
+        if not self.it_status:
+            target_distance *= -1
+            target_angle = np.pi - target_angle
+            if target_angle > np.pi:
+                target_angle -= 2*np.pi
+            target_angle *= -1
         print(target_angle, target_distance)
-        self.drive(target_angle,target_distance)
+        
+        drive_msg.linear.x = np.clip(float(FORWARD_CONSTANT * target_distance), -1.0, 1.0)    #Determines final linear drive message
+        drive_msg.angular.z = np.clip(float(TURN_CONSTANT * target_angle), -1.0, 1.0)         #Determines final angular drive message
+        self.pub.publish(drive_msg) #Commands Neato motors
     
     def check_it_status(self,msg):
         if msg.data==self.neato_id:
@@ -74,21 +85,6 @@ class Navigator(Node):
         dist_sq = cart_x ** 2 + cart_y ** 2
         source_deriv = 2 * cartesian / (dist_sq ** 2) / cartesian.shape[1] * LIDAR_ALPHA
         self.lidar_gradient = -np.sum(source_deriv, axis=1)
-        
-    def drive(self,angle, distance):
-        """
-        Takes in angle and distance to the nearest neato and commands Neato motors to head towards or away from that position
-        """
-        if self.it_status:
-            forward_constant=.1
-        else:
-            forward_constant=-.1
-        drive_msg = Twist()
-        drive_msg.linear.x = np.clip(float(forward_constant * distance), -1.0, 1.0)    #Determines final linear drive message
-        drive_msg.angular.z = np.clip(float(TURN_CONSTANT*angle), -1.0, 1.0)         #Determines final angular drive message
-        print(drive_msg.linear.x, drive_msg.angular.z)
-        self.pub.publish(drive_msg) #Commands Neato motors
-        return
         
 
 def main(args=None):
